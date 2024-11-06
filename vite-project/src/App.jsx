5861d8d19dd57952c7782377db1977cdcce2563a
Daniel-505 <fericos190@gmail.com>
2024-11-06 16:56:35 -0300

    avanze con la pagina web

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -8,9 +6,38 @@ function App() {
 
   return (
     <>
-      <header>Alan Sutton y Las Criaturitas de la Anciedad</header>
+      <header>
+        <h1 className="Texto1">Alan Sutton y Las Criaturitas de la Anciedad</h1>
+        </header>
       <div>
-        <div></div>
+        <div className="centrar">
+            <a href="https://open.spotify.com/intl-es/album/2dJfWjL7AHKli4i7MjCp3E">
+            <img className="imagen" src="/public/1.png" alt="" />
+            </a>
+
+            <a href="https://open.spotify.com/intl-es/album/2iKR4ZBb3NLJM0Zg8yhQvi">
+            <img className="imagen" src="/public/2.png" alt="" />
+            </a>
+
+            <a href="https://open.spotify.com/intl-es/album/7hgxTp99yKwlzXDZgV6O8I">
+            <img className="imagen" src="/public/3.png" alt="" />
+            </a>
+
+            <a href="https://open.spotify.com/intl-es/album/3i0EZnMVNrETaQcHKs3bib">
+            <img className="imagen" src="/public/4.png" alt="" />
+            </a>
+
+            <a href="https://open.spotify.com/intl-es/album/4Wlrq1ImA0Se5pJ0BiB4iL">
+            <img className="imagen" src="/public/5.png" alt="" />
+            </a>
+        </div>
+        
+        <div>
+          <div>
+              <li className="Textocolor" >1</li>
+              
+          </div>
+        </div>
       </div>
     </>
   );
