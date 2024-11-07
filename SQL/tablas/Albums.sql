a7463d1c256954e60868cdef87c9ed24b7374a9e
daiAPZ <azdelugano001mela@gmail.com>
2024-11-07 11:13:41 -0300

    hace la tablas y los insert de todas las tablas menos de la tabla canciones

---
+++ SQL/tablas/Albums.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Albums (
+    id_album INT PRIMARY KEY AUTO_INCREMENT,
+    id_artista INT,
+    titulo VARCHAR(255) NOT NULL,
+    Año_lanzamiento INT,
+    genero VARCHAR(100),
+    FOREIGN KEY (id_artista) REFERENCES Artistas(id_artista)
+);
+
+INSERT INTO Albumes (id_artista, titulo, Año_lanzamiento, genero)
+VALUES 
+(1, 'Alan Sutton y las Criaturitas de la Anciedad', 2018, 'Rock Alternativo'),
+(1, 'Hombrecito con los pies en la Tierra', 2020, 'Rock Alternativo'),
+(1, 'Algo tiene que cambiar', 2023, 'Rock Alternativo');
