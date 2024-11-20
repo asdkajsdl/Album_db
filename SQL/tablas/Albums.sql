ecd11e0e2be3891264598fdb61b2a70635cc4c59
fericos190@gmail.com <fericos190@gmail.com>
2024-11-20 12:09:18 -0300

    configure app para que muestre la basadedatos en la pagina

--- SQL/tablas/Albums.sql
+++ SQL/tablas/Albums.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Albums (
-    id_album INT PRIMARY KEY AUTO_INCREMENT,
+    id_album INT PRIMARY KEY,
     id_artista INT,
     titulo VARCHAR(255) NOT NULL,
     Año_lanzamiento INT,
@@ -8,10 +8,11 @@ CREATE TABLE Albums (
 );
 
 
-INSERT INTO Albums (id_artista, titulo, Año_lanzamiento, genero)
-VALUES 
-(1, 'Alan Sutton y las Criaturitas de la Anciedad', 2018, 'Rock Alternativo'),
-(1, 'Hombrecito con los pies en la Tierra', 2020, 'Rock Alternativo'),
-(1, 'Algo tiene que cambiar', 2023, 'Rock Alternativo'),
-(1, 'Tomate 5', 2021, 'Rock Alternativo');
+INSERT INTO Albums (id_album, id_artista, titulo, Año_lanzamiento, genero)
+VALUES
+(1, 1, 'Alan Sutton y las Criaturitas de la Anciedad', 2018, 'Rock Alternativo'),
+(2, 1, 'Hombrecito con los pies en la Tierra', 2020, 'Rock Alternativo'),
+(3, 1, 'Algo tiene que cambiar', 2023, 'Rock Alternativo'),
+(4, 1, 'Tomate 5', 2021, 'Rock Alternativo');
+
   
